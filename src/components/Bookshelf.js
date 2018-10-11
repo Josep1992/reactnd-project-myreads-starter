265aca192c5cdc5bbbd162b39b413f62f15b87fd
Josep López <josep-lopez@live.com>
2018-10-11 01:53:08 -0400

    Ui renders. Left are books to render in the shelfs

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Book from './Book';
 
 class Bookshelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  };
+
   render() {
+    const { books } = this.props;
+    this.props.books.map((book) => console.log(book));
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
+            {books.length !== 0
+              ? books.map((book) => {
+                  <Book info={book} key={book.id} />;
+                })
+              : console.log('Books are empty')}
           </ol>
         </div>
       </div>
--- src/components/Index.js
+++ src/components/Index.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 class Index extends Component {
   render() {
-    const { books } = this.props;
+    const { allBookData } = this.props;
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <Bookshelf books={books} />
+          <Bookshelf books={allBookData} />
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
