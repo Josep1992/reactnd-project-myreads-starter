cd8ae4aa3acb52eb7380eb3d80bc5e2b002f041d
Josep López <josep-lopez@live.com>
2018-10-12 10:20:56 -0400

    changed to functional component

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-class Bookshelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onChangeReadingState: PropTypes.func.isRequired,
-    tier: PropTypes.string.isRequired,
-  };
+const Bookshelf = (props) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{props.tier}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.books.length !== 0
+          ? props.books.map((book) => (
+              <Book
+                book={book}
+                key={book.id}
+                onChangeReadingState={props.onChangeReadingState}
+              />
+            ))
+          : console.log('Books are empty')}
+      </ol>
+    </div>
+  </div>
+);
 
-  render() {
-    const { books, tier, onChangeReadingState } = this.props;
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{tier}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.length !== 0
-              ? books.map((book) => (
-                  <Book
-                    book={book}
-                    key={book.id}
-                    onChangeReadingState={onChangeReadingState}
-                  />
-                ))
-              : console.log('Books are empty')}
-          </ol>
-        </div>
-      </div>
-    );
-  }
-}
+Bookshelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  onChangeReadingState: PropTypes.func.isRequired,
+  tier: PropTypes.string.isRequired,
+};
 
 export default Bookshelf;
