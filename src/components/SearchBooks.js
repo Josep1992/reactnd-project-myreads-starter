5a23eb237d1d5b1d6368f5a24eeb09f0dafdee1f
Josep López <josep-lopez@live.com>
2018-10-12 06:35:29 -0400

    minor changes

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -4,9 +4,6 @@ import Loader from './Loader';
 import Book from './Book';
 import { Link } from 'react-router-dom';
 
-/* https://www.youtube.com/watch?v=acJHkd6K5kI&t=0s&list=PLKC17wty6rS1XVZbRlWjYU0WVsIoJyO3s&index=4%2A%2F
-   searchAllBooks function Tutorial Requests: FEND Project 6 - Walk Through (LONG) Ryan Waite
-*/
 class SearchBooks extends Component {
   state = {
     query: '',
@@ -19,32 +16,27 @@ class SearchBooks extends Component {
     this.setState({ books: this.props.location.state.books });
   };
 
+  /* https://www.youtube.com/watch?v=acJHkd6K5kI&t=0s&list=PLKC17wty6rS1XVZbRlWjYU0WVsIoJyO3s&index=4%2A%2F
+   searchAllBooks && handleSearch  function were based of Tutorial Requests did some changes: FEND Project 6 - Walk Through (LONG) Ryan Waite
+*/
   searchAllBooks = () => {
-    if (this.state.query === '' || this.state.query === undefined) {
-      return this.setState({ search: [] });
-    } else {
-      BooksAPI.search(this.state.query.trim())
-        .then((response) => {
-          if (response.error) {
-            return this.setState({ search: [] });
-          } else {
-            this.setState({ loading: true });
-            response.forEach((bk) => {
-              let find = this.state.books.filter((book) => book.id === bk.id);
-              if (find[0]) {
-                bk.shelf = find[0].shelf;
-              }
-            });
-            return this.setState({
-              search: response,
-              loading: setTimeout(() => {
-                this.setState({ loading: false });
-              }, 2000),
-            });
+    BooksAPI.search(this.state.query.trim())
+      .then((response) => {
+        this.setState({ loading: true });
+        response.forEach((bk) => {
+          let find = this.state.books.filter((book) => book.id === bk.id);
+          if (find[0]) {
+            bk.shelf = find[0].shelf;
           }
-        })
-        .catch((error) => console.error(error));
-    }
+        });
+        return this.setState({
+          search: response,
+          loading: setTimeout(() => {
+            this.setState({ loading: false });
+          }, 2000),
+        });
+      })
+      .catch((error) => console.error(error));
   };
 
   handleSearch = (query) => {
